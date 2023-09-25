57946d32f2cffe7ad3b4fb5c11e336dfb893e4e6
RenanPiccinini <renanpiccinini1990@gmail.com>
2023-09-25 19:21:50 -0300

    deletar-cadastro-ok

--- resources/views/cadastro/cadastros.blade.php
+++ resources/views/cadastro/cadastros.blade.php
@@ -22,9 +22,9 @@
                                         <th>E-mail</th>
                                         <th>Telefone</th>
                                         <th>Editar</th>
+                                        <th>Excluir</th>
                                     </tr>
                                 </thead>
-
                                 <tbody>
                                     @foreach($cadastros as $cadastro)
                                         <tr>
@@ -36,9 +36,39 @@
                                                     <a class="btn btn-warning" href="{{ route('editar-cadastro', $cadastro->id)}}">Editar</a>
                                                 </div>
                                             </td>
-                                        </tr>
-                                    @endforeach
+                                            <td>
+                                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal{{ $cadastro->id }}">
+                                                    Deletar
+                                                </button>
+                                            </td>
+                                          </tr>
 
+                                        <!-- Modal de confirmação -->
+                                        <div class="modal fade" id="confirmDeleteModal{{ $cadastro->id }}" tabindex="-1" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmação de Exclusão</h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <p>Você tem certeza que deseja deletar o cadastro?</p>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                                                        <form action="{{ route('deletar-cadastro', $cadastro->id) }}" method="POST" class="d-inline">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-danger">Deletar</button>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        </tr>
+
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
